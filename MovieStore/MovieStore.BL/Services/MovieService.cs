81e1c6bf095427264a722fc37955210eb5baa68b
Boris Serafimov <b.b.serafimov@gmail.com>
2024-11-11 18:27:52 +0200

    AddMovie

--- MovieStore/MovieStore.BL/Services/MovieService.cs
+++ MovieStore/MovieStore.BL/Services/MovieService.cs
@@ -8,9 +8,12 @@ namespace MovieStore.BL.Services
     {
         private readonly IMovieRepository _movieRepository;
 
+        public Guid Id { get; set; }
+
         public MovieService(IMovieRepository movieRepository)
         {
             _movieRepository = movieRepository;
+            Id = Guid.NewGuid();
         }
         
         public List<Movie> GetAllMovies()
